7da46e950b2eab14cada1375a5fd36dfc783ffdf
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-10-28 17:44:35 +0100

    Añadir consultas de buscar por autor, buscar por autor y titulo y buscar por autor y que alguna palabra coincida con titulo

--- notes/src/main/java/com/notes/notes/repository/NotesRepository.java
+++ notes/src/main/java/com/notes/notes/repository/NotesRepository.java
@@ -1,9 +1,15 @@
 package com.notes.notes.repository;
 
 
+import com.notes.notes.model.AppUser;
 import com.notes.notes.model.NotesModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotesRepository extends JpaRepository<NotesModel, Long>{
+    List<NotesModel> findByAuthorUsername(String username);
+    List<NotesModel> findByTitleAndAuthor(String title, AppUser author);
+    List<NotesModel> findByTitleContainingIgnoreCaseAndAuthor(String title, AppUser author);
 
 }

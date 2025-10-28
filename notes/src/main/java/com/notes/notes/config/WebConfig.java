02edaf21fe0f8df45dc35d38c953b2052494315e
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-10-28 17:40:05 +0100

    Hacer que los delete y update sean como post

---
+++ notes/src/main/java/com/notes/notes/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.notes.notes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+public class WebConfig {
+
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}

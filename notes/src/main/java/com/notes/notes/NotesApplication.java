eece0a56591748e0539f1e2b089abedb4e7dfc32
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-10-28 18:32:15 +0100

    Añadir para que sepa donde esta el .env y usarlo

--- notes/src/main/java/com/notes/notes/NotesApplication.java
+++ notes/src/main/java/com/notes/notes/NotesApplication.java
@@ -1,5 +1,6 @@
 package com.notes.notes;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotesApplication.class, args);
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./notes").load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+        SpringApplication.run(NotesApplication.class, args);
 	}
 
 }

4abfbe0cad055dee56573dcf06519ba850f954da
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-10-30 18:30:01 +0100

    Modificar DTO para tenerlo despues preparado

--- notes/src/main/java/com/notes/notes/dto/RegisterDto.java
+++ notes/src/main/java/com/notes/notes/dto/RegisterDto.java
@@ -2,6 +2,8 @@ package com.notes.notes.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record RegisterDto(@NotBlank String username, @NotBlank String password) {
-
-}
+public record RegisterDto(
+        @NotBlank String username,
+        @NotBlank String password
+)
+{}

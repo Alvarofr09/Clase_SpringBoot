dfa2cb6fd11b599e6d9dc0e44c79b921a86f78e0
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-10-30 18:28:30 +0100

    2 Excepciones nuevas (UnauthorizedAccessExeption y General) y usar en todas la nueva plantilla

--- notes/src/main/java/com/notes/notes/exception/GlobalExceptionHandler.java
+++ notes/src/main/java/com/notes/notes/exception/GlobalExceptionHandler.java
@@ -11,8 +11,8 @@ public class GlobalExceptionHandler {
     // Maneja la excepción cuando una tarea no se encuentra
     @ExceptionHandler(NoteNotFoundException.class)
     public ModelAndView handleNoteNotFound(NoteNotFoundException ex) {
-        // 1. Crear un ModelAndView. Usaremos una nueva platinlla: 'error-404'
-        ModelAndView modelAndView = new ModelAndView("error-404");
+        // 1. Crear un ModelAndView. Usaremos la plantilla: 'error'
+        ModelAndView modelAndView = new ModelAndView("error");
 
         // 2. Añadir el mensaje de error al modelo.
         modelAndView.addObject("errorMessage", ex.getMessage());
@@ -20,6 +20,37 @@ public class GlobalExceptionHandler {
 
         // 3. Establecer el codigo de estado HTTP 404.
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
+
+        return modelAndView;
+    }
+
+    // Maneja la excepción cuando un usuario intenta editar una nota de otra persona
+    @ExceptionHandler(UnauthorizedAccessException.class)
+    public ModelAndView UnathorizedAccess(UnauthorizedAccessException ex) {
+        // 1. Crear un ModelAndView. Usaremos la plantilla: 'error'
+        ModelAndView modelAndView = new ModelAndView("error");
+
+        // 2. Añadir el mensaje de error al modelo.
+        modelAndView.addObject("errorMessage", ex.getMessage());
+        modelAndView.addObject("status", HttpStatus.METHOD_NOT_ALLOWED.value()); // Código 405
+
+        // 3. Establecer el codigo de estado HTTP 405.
+        modelAndView.setStatus(HttpStatus.METHOD_NOT_ALLOWED);
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleGeneral(Exception ex) {
+        // 1. Crear un ModelAndView. Usaremos la plantilla: 'error'
+        ModelAndView modelAndView = new ModelAndView("error");
+
+        // 2. Añadir el mensaje de error al modelo.
+        modelAndView.addObject("errorMessage", "Error interno del servidor");
+        modelAndView.addObject("status", HttpStatus.INTERNAL_SERVER_ERROR.value()); // Código 500
+
+        // 3. Establecer el codigo de estado HTTP 500.
+        modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
 
         return modelAndView;
     }

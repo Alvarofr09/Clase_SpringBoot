ea760602bad9de32e78a7678edb6696efbdc617f
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-11-06 17:47:53 +0100

    New Exception

---
+++ notes/src/main/java/com/notes/notes/exception/DuplicateUsernameException.java
@@ -0,0 +1,7 @@
+package com.notes.notes.exception;
+
+public class DuplicateUsernameException extends RuntimeException {
+    public DuplicateUsernameException(String message) {
+        super(message);
+    }
+}

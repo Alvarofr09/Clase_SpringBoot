2741a8553897200e8cf79215d089be90ceccd646
Alvarofr09 <alvarofriasruiz03@gmail.com>
2025-11-06 17:49:33 +0100

    Logic of register

---
+++ notes/src/main/java/com/notes/notes/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.notes.notes.service;
+
+import com.notes.notes.exception.DuplicateUsernameException;
+import com.notes.notes.model.AppUser;
+import com.notes.notes.repository.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AuthService {
+    private final AppUserRepository repository;
+    private final PasswordEncoder encoder;
+
+    @Autowired
+    public AuthService(AppUserRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
+
+    public void register(String username, String rawPassword) {
+        // validacion si existe el usuario
+        if (repository.existsByUsername(username)) {
+            throw new DuplicateUsernameException("El usuario ya existe");
+        }
+        AppUser u = new AppUser();
+        u.setUsername(username);
+        u.setPassword(encoder.encode(rawPassword));
+        u.setRole("ROLE_USER");
+
+        repository.save(u);
+    }
+}
